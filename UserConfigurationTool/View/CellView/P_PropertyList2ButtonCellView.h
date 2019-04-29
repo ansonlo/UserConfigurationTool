ae1bc192e08dfc2a3508a7a46aedff7bb3d4c789
lincf0912 <dayflyking@163.com>
2019-04-29 18:17:11 +0800

    -新增代理

--- UserConfigurationTool/View/CellView/P_PropertyList2ButtonCellView.h
+++ UserConfigurationTool/View/CellView/P_PropertyList2ButtonCellView.h
@@ -9,8 +9,19 @@
 #import "P_PropertyListBasicCellView.h"
 
 NS_ASSUME_NONNULL_BEGIN
+@class P_PropertyList2ButtonCellView;
+
+@protocol P_PropertyList2ButtonCellViewDelegate <P_PropertyListCellViewDelegate>
+
+@optional
+- (id)p_propertyList2ButtonCellPlusAction:(P_PropertyList2ButtonCellView *)cellView;
+- (id)p_propertyList2ButtonCellMinusAction:(P_PropertyList2ButtonCellView *)cellView;
+
+@end
 
 @interface P_PropertyList2ButtonCellView : P_PropertyListBasicCellView
+
+@property (nonatomic, weak) id<P_PropertyList2ButtonCellViewDelegate> delegate;
 
 - (void)p_setShowsControlButtons:(BOOL)showsControlButtons addButtonEnabled:(BOOL)addButtonEnabled deleteButtonEnabled:(BOOL)deleteButtonEnabled;
 
